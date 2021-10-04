aff18e1f6a34ba8190c85fd87b3063e16275990a
Nick Valencia <valencian1@wit.edu>
2021-10-04 09:18:15 -0400

    Add files via upload

---
+++ DS - 1 Bag App - Group 14 valencian1/src/edu/wit/scds/comp2000/bag/app/GroceryBagger.java
@@ -0,0 +1,23 @@
+package edu.wit.scds.comp2000.bag.app;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+public class GroceryBagger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner groceryList = new Scanner(new FileReader("./data/groceries.txt")
+		while (groceryList.hasNextLine()) {
+			
+			String groceries = groceryList.nextLine();
+			
+		}
+		
+		
+		
+	
+
+	}
+
+}
