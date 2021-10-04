48ae43cb3c025f70276e6fe2c4b02dd033102327
nicholsa2 <49592929+nicholsa2@users.noreply.github.com>
2021-10-04 11:39:08 -0400

    Update GroceryBagger.java

--- DS - 1 Bag App - Group 14 valencian1/src/edu/wit/scds/comp2000/bag/app/GroceryBagger.java
+++ DS - 1 Bag App - Group 14 valencian1/src/edu/wit/scds/comp2000/bag/app/GroceryBagger.java
@@ -1,23 +1,66 @@
 package edu.wit.scds.comp2000.bag.app;
-import java.io.FileReader;
+
 import java.util.Scanner;
 
+import java.io.BufferedReader;
 
-public class GroceryBagger {
+import java.io.FileReader;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner groceryList = new Scanner(new FileReader("./data/groceries.txt")
-		while (groceryList.hasNextLine()) {
-			
-			String groceries = groceryList.nextLine();
-			
-		}
-		
-		
-		
+import java.io.FileNotFoundException;
+
+import edu.wit.scds.comp2000.bag.BagInterface ;
+
+import edu.wit.scds.comp2000.bag.adt.ResizableArrayBag;
+
+public class GroceryBagger{
 	
+	public static void main( final String[] args ) throws FileNotFoundException {
+		/**
+		 * Scan through data line by line to grab each item
+		 * Run certain attributes through enums to make them easier to use
+		 * Check existing bags for compatibility, create new ones if needed 
+		 */
+		Scanner conveyorBelt =  new Scanner(  new BufferedReader( new FileReader("./data/groceries.txt" ) ) ) ;
+		
+		
+		int numberOfItems = 0 ;
+		
+		while ( conveyorBelt.hasNextLine() )
+	     {
+			String itemFromConveyorBelt = conveyorBelt.nextLine() ; 
+			String [] itemAttributes = itemFromConveyorBelt.split( "\t" );
+						
+			/**
+			 * Assign attributes to variables
+			 * Attribute order is: size, weight, hardness, rigidity, breakability, perishiability
+			 */
+			
+			String size = itemAttributes[1] ;
+			String weight = itemAttributes[2] ;
+			String hardness = itemAttributes[3] ;
+			String rigidity = itemAttributes[4] ;
+			String breakable = itemAttributes[5] ;
+			String perishable = itemAttributes[6] ;
+			
+			GroceryItemSize enumSize = GroceryItemSize.interpretDescription( size ) ;
+			
+			/*
+			 * 
+			 */
+
+			
+	     }
+
 
 	}
+	
+	// Checks if items of a certain attribute have been already added to each bag
+	// finalResult indicates what method contains has returned
+	private static void checkAttribute( final BagInterface<String> aBag, 
+										final String test)
+		{
+		aBag.contains( test ); 
+		}
+	
 
 }
